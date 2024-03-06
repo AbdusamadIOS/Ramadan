b391fac42727a54338d8c12e6fab2d9c8c1804eb
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-03-06 22:22:56 +0500

    Initial Commit

---
+++ Ramazon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ramazon
+//
+//  Created by Abdusamad Mamasoliyev on 06/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
